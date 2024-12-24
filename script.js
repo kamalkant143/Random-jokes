1632e4dcbcc2474e0ed42f6e52aa8874347b02e7
kamal kant <kamalshakya14311@gmail.com>
2024-12-24 18:04:39 +0530

    new changing

--- script.js
+++ script.js
@@ -3,10 +3,12 @@ let btn = document.querySelector(".btn")
 let api='https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,political,racist,sexist,explicit'
 
 let randomJokes=()=>{
+    jokes.classList.remove("box")
    fetch(api).then((res)=>res.json())
    .then((data)=>{
    console.log( jokes.textContent=`${data.joke}`);
-   
+   jokes.classList.add("box")
+}).catch((error)=>{console.log(error);
 })
 
    
